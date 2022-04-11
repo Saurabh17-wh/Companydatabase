df41452e8eb9d72515a3b9fb218bb172073a612b
Saurabh17-wh <75580992+Saurabh17-wh@users.noreply.github.com>
2022-04-11 15:47:17 +0530

    Delete company directory

--- company/src/main/java/com/info/company/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.info.company.dao;
-
-import com.info.company.entities.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User , Integer>
-{
-    public User findById(int id);
-}
